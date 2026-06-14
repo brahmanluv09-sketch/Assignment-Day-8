25106297de8234343017b960c76ef74b6b25366b
brahmanluv09-sketch <brahmanluv09@gmail.com>
2026-06-14 23:49:34 +0530

    Add files via upload
    Completed Day 8 of the 30 Days C Programming Challenge.
    
    Programs solved:
    
    1. Half Pyramid Pattern
    2. Number Triangle Pattern
    3. Character Triangle Pattern
    4. Repeated Number Pattern

---
+++ ques31.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (char ch = 'A'; ch < 'A' + i; ch++)
+        {
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
